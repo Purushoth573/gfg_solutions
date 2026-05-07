2831dbcd5c3bdee4b0846ea152db6e3ff8396516
PURUSHOTHAMAN K <purushothk573@gmail.com>
2026-05-07 23:13:04 +0530

    Update max-sum-subarray-of-size-k.py

--- Difficulty: Easy/Max Sum Subarray of size K/max-sum-subarray-of-size-k.py
+++ Difficulty: Easy/Max Sum Subarray of size K/max-sum-subarray-of-size-k.py
@@ -1,12 +1,30 @@
 class Solution:
     def maxSubarraySum(self, arr, k):
-        l=0
-        res=0
-        cursum=0
+        l = 0
+        res = 0
+        cursum = 0
+
         for r in range(len(arr)):
-            cursum+=arr[r]
-            res=max(res,cursum)
-            while r-l+1 >=k:
-                cursum-=arr[l]
-                l+=1
+            cursum += arr[r]
+            res = max(res, cursum)
+
+            while r - l + 1 >= k:
+                cursum -= arr[l]
+                l += 1
+
         return res
+
+
+# Input
+arr = [2, 1, 5, 1, 3, 2]
+k = 3
+
+print("Running Maximum Subarray Sum Program")
+print("Input Array:", arr)
+print("Window Size:", k)
+
+obj = Solution()
+
+result = obj.maxSubarraySum(arr, k)
+
+print("Maximum Subarray Sum:", result)
